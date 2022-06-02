22409e99c0ab3dbe44eb574f614dfb68eeb7307c
Hasan Shanto <hasanshanto922@gmail.com>
2022-06-03 00:46:28 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './component/Header/Header';
+import Shop from './component/Shop/Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+
+export default App;
